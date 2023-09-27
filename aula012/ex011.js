7ef26b3565a9b9ebb87089a156d2fefd1c9cc141
viniciusmartines <142375067+viniciusmartines@users.noreply.github.com>
2023-09-27 15:55:49 -0300

    ex013 js

---
+++ aula012/ex011.js
@@ -0,0 +1,9 @@
+let idade = 70
+console.log('Você tem ' +idade+ ' anos.')
+if (idade >= 16 && idade < 18) {
+    console.log('Voto opcional')
+} else if (idade >= 18 && idade <= 65) {
+    console.log('Voto obrigatório')
+} else {
+    console.log('Não vota')
+}
