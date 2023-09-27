2c0ee0255652c8684901270a6b86ede097475835
viniciusmartines <142375067+viniciusmartines@users.noreply.github.com>
2023-09-27 15:02:14 -0300

    ex010 js

---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+let velo = 90
+console.log(`A velocidade do seu carro é ${velo}Km/h`)
+if (velo > 60) {
+    console.log(`VOCÊ ESTÁ ACIMA DO LIMITE PERMITIDO. MULTADO!`)
+} else {
+    console.log(`Você está abaixo do limite de velocidade. PARABÉNS!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
