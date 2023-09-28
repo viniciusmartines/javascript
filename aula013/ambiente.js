492ed530436374b744996ded6d1d6d1717eb3081
viniciusmartines <142375067+viniciusmartines@users.noreply.github.com>
2023-09-28 15:56:59 -0300

    repetições em js

---
+++ aula013/ambiente.js
@@ -0,0 +1,22 @@
+let cont = 1
+do {
+    console.log ('Passo ' + cont)
+    cont++
+} while(cont <= 6)
+
+/*
+let cont = 1
+while (cont <= 6) {
+    console.log('Passo ' + cont)
+    cont++
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ aula014/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Vai começar...')
+for(let cont = 1;cont <= 4;cont++) {
+    console.log('Passo ' + cont)
+}
+console.log('FIM!')
