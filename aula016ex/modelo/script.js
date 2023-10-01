645decf205bb57531412057a8a6a0e323aa40875
viniciusmartines <142375067+viniciusmartines@users.noreply.github.com>
2023-10-01 05:08:12 -0300

    finalizei o curso js gustavo guanabara

--- aula016ex/modelo/script.js
+++ aula016ex/modelo/script.js
@@ -1,29 +1,73 @@
-let qntd = 0
-let soma = 0
-let txtf = document.getElementById("numer")
-let txts = document.getElementById("sele")
-let opt = document.createElement('option')
-function adicion() {
-    let num = Number(txtf.value)
-    if ((num.length == 0) || (num < 1 || num > 100)) {
-        alert(`DIGITE UM VALOR DENTRO DOS PARÂMETROS!`)
+let txtf = document.getElementById('numer')
+let txts = document.getElementById('sele')
+let resp = document.getElementById('resp')
+let valores = []
+
+function isnumero(v) {
+    if(Number(v) >= 1 && Number(v) <= 100) {
+        return true
     } else {
-        opt.innerHTML = `Valor ${num} adicionado<br>`
-        txts.appendChild(opt)
-        qntd = qntd + 1
-        soma = soma + num
-        media = soma/qntd
+        return false
     }
 }
 
+function inlista(v, l) {
+    if(l.indexOf(Number(v)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
 
+function adicion() {
+    if(isnumero(txtf.value) && !inlista(txtf.value, valores)) {
+        valores.push(Number(txtf.value))
+        let opt = document.createElement('option')
+        opt.text = `Valor ${txtf.value} adicionado.`
+        txts.appendChild(opt)
+        resp.innerHTML = ''
+    } else {
+        alert("Valor inválido ou já adicionado na lista.")
+    }
+    txtf.value = ''
+    txtf.focus()
+}
+
+function analisa(x) {
+    if (x.length == 0) {
+        return true
+    } else {
+        return false
+    }
+}
 
 function final() {
-    let answ = document.getElementById("resp")
-    answ.innerHTML = `Ao todo, temos ${qntd} números cadastrados<br>`
-    answ.innerHTML += `O maior valor informado foi <br>`
-    answ.innerHTML += `O menor valor informado foi <br>`
-    answ.innerHTML += `Somando todos os valores, temos ${soma}<br>`
-    answ.innerHTML += `A média dos valores digitados é  ${media}<br>`
-    answ.style.lineHeight = "40px"
+    if(!analisa(valores)) {
+        // valores.sort()
+        let qntd = valores.length
+        // let menor = valores[0] EU QUE FIZ
+        // let maior = valores[(valores.length) -1]
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let cont in valores) {
+            soma += valores[cont]
+            if (valores[cont] > maior) {
+                maior = valores[cont]
+            }
+            if (valores[cont] < menor) {
+                menor = valores[cont]
+            }
+        }
+        media = soma/qntd
+        resp.innerHTML = ''
+        resp.innerHTML += `<p>Ao todo, temos ${qntd} números cadastrados.</p>`
+        resp.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        resp.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        resp.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        resp.innerHTML += `<p>A média dos valores digitadso é ${media}.</p>`
+    } else {
+        alert("Adicione valores antes de finalizar!")
+    }
 }
